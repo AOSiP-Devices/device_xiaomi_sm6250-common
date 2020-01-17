1b0980995dba2633a8025928596198bba5caad6b
Arthur Ishiguro <arthuri@google.com>
2020-01-17 14:49:19 -0800

    sm6250-common: Add the Context Hub feature
    Bug: 147677675
    Test: Compile only
    Change-Id: I76b911c5991d72490705bcb3249638f923bff5c0

--- common.mk
+++ common.mk
@@ -41,6 +41,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
+# Context Hub
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.context_hub.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.context_hub.xml
+
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
