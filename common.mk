232be38e463c61f15dd51b3ba753acd105418fff
Chen Chen <cncn@google.com>
2020-02-25 11:57:24 -0800

    sm6250-common: AFH: Modify BT services for channel avoidance
    Test: 1) Manual atest; 2) Running coexist testing utility and check log
    Bug: 143146617
    
    Change-Id: Ie9722fd370a3400a239c70632e6b261a05c60969

--- common.mk
+++ common.mk
@@ -65,6 +65,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
+# Bluetooth AFH
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.bt_channel_avoidance@1.0-impl
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
