843e1b3da6a27448155aac038fadf94932e380ed
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2020-08-14 20:26:17 +0530

    sm6250-common: lineage->aosip
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@gmail.com>

--- common.mk
+++ common.mk
@@ -144,13 +144,12 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay-aosip \
     $(LOCAL_PATH)/overlay-system
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
-    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap \
+    $(LOCAL_PATH)/overlay-aosip/packages/apps/Snap \
     $(LOCAL_PATH)/overlay-system
 
 # Partitions
