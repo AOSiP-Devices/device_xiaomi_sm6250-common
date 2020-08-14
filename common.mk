c7552ef2f9c9f7db01cda794eb6df51e6e848a15
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2020-08-14 20:37:36 +0530

    sm6250-common: Include Google PSU if exists
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@gmail.com>

--- common.mk
+++ common.mk
@@ -10,6 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit proprietary targets
 $(call inherit-product-if-exists, vendor/xiaomi/sm6250-common/sm6250-common-vendor.mk)
 
+# Inherit PSU
+$(call inherit-product-if-exists, vendor/google/psu/google-psu.mk)
+
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
