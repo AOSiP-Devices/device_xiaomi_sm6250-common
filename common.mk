81a9a0f999fc6b788b1e02b333e9a07cac0dd00b
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2020-08-26 12:32:27 +0530

    sm6250-common: Add power stats HAL from sunfish
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@gmail.com>

--- common.mk
+++ common.mk
@@ -163,7 +163,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.xiaomi_sm6250-libperfmgr
+    android.hardware.power@1.3-service.xiaomi_sm6250-libperfmgr \
+    android.hardware.power.stats@1.0-service.xiaomi_sm6250
 
 # RCS
 PRODUCT_PACKAGES += \
@@ -189,7 +190,8 @@ PRODUCT_COPY_FILES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/google/pixel
 
 # Telephony
 PRODUCT_PACKAGES += \
